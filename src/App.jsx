52e9e332e6481b09bb2715a4a49967bde13b6db1
Varunkumar0812 <varunkumarceg@gmail.com>
2024-02-17 18:10:29 +0530

    About, Celebrity

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,29 @@
+import About from "../components/About";
+import CelebrityCard from "../components/CelebrityCard";
+
 const Home = () => {
-	return (
-		<div>
-			<div className="min-h-screen flex justify-center items-center bg-sea1 bg-fixed bg-no-repeat bg-cover bg-center">
-				<h1 className="font-black text-6xl">Parallax</h1>
-			</div>
+  return (
+    <div>
+      <div className="min-h-screen flex justify-center items-center bg-sea1 bg-fixed bg-no-repeat bg-cover bg-center">
+        <h1 className="font-black text-6xl">Parallax</h1>
+      </div>
 
-			<div className="p-10">
-				<h2 className="font-bold text-4xl">Sample Section</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab numquam eveniet molestias veniam, inventore sint voluptates nam provident impedit laborum ipsa vel quos amet laboriosam dolorum
-					suscipit! Nihil eos repellendus blanditiis repellat laboriosam veniam quod maxime ab! Ea eveniet doloremque, excepturi totam, et molestias dicta accusamus quibusdam quas sunt inventore!
-				</p>
-			</div>
+      <div className="p-10">
+        <About />
+      </div>
 
-			<div className="bg-sea2 min-h-screen bg-center bg-no-repeat bg-cover bg-fixed"></div>
+      <div className="bg-sea2 min-h-screen bg-center bg-no-repeat bg-cover bg-fixed">
+        <div className="mt-20">
+          <div className="text-center text-6xl">Celebrities </div>
+          <div className="mt-20 w-full flex justify-center items-center">
+            <CelebrityCard key="card1" />
+          </div>
+        </div>
+      </div>
 
-			<div className="bg-sea3 min-h-screen bg-fixed bg-no-repeat bg-cover"></div>
-		</div>
-	);
+      <div className="bg-sea3 min-h-screen bg-fixed bg-no-repeat bg-cover"></div>
+    </div>
+  );
 };
 
 export default Home;
