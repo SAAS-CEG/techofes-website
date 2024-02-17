9bf234c526772855350137833b1621ce884ac955
Vimalesh C T <pranovvimal30@gmail.com>
2024-02-18 01:24:07 +0530

    main carousal

--- src/components/mainCarousel/Slider.jsx
+++ src/components/mainCarousel/Slider.jsx
@@ -1,62 +1,59 @@
 import "./Maincaro.css";
+// import img3 from "../../assets/img3.jpeg";
+// import img4 from "../../assets/img4.jpeg";
+// import img5 from "../../assets/img5.jpeg";
+// import img6 from "../../assets/img6.jpg";
 const Slider = () => {
-  <div className="body">
-    <div className="wrapper">
-      <div className="carousel" tabindex="0">
-        <div className="slide">
-          <a href="#item-4">Slide 4</a>
-          <img src="https://source.unsplash.com/random/800x500?1" />
-          <a href="#item-2">Slide 2</a>
+  <div className="carousel">
+    <ul className="slides">
+      <input type="radio" name="radio-buttons" id="img-1" checked />
+      <li className="slide-container">
+        <div className="slide-image">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/9e/Timisoara_-_Regional_Business_Centre.jpg" />
         </div>
-        <div className="slide">
-          <a href="#item-1">Slide 1</a>
-          <img id="item-2" src="https://source.unsplash.com/random/800x500?2" />
-          <a href="#item-3">Slide 3</a>
+        <div className="carousel-controls">
+          <label for="img-3" className="prev-slide">
+            <span>&lsaquo;</span>
+          </label>
+          <label for="img-2" className="next-slide">
+            <span>&rsaquo;</span>
+          </label>
         </div>
-        <div className="slide">
-          <a href="#item-2">Slide 2</a>
-          <img id="item-3" src="https://source.unsplash.com/random/800x500?3" />
-          <a href="#item-4">Slide 4</a>
+      </li>
+      <input type="radio" name="radio-buttons" id="img-2" />
+      <li className="slide-container">
+        <div className="slide-image">
+          <img src="https://content.r9cdn.net/rimg/dimg/db/02/06b291e8-city-14912-171317ad83a.jpg?width=1750&height=1000&xhint=3040&yhint=2553&crop=true" />
         </div>
-        <div className="slide">
-          <a href="#item-3">Slide 3</a>
-          <img id="item-4" src="https://source.unsplash.com/random/800x500?4" />
-          <a href="#item-1">Slide 1</a>
+        <div className="carousel-controls">
+          <label for="img-1" className="prev-slide">
+            <span>&lsaquo;</span>
+          </label>
+          <label for="img-3" className="next-slide">
+            <span>&rsaquo;</span>
+          </label>
         </div>
+      </li>
+      <input type="radio" name="radio-buttons" id="img-3" />
+      <li className="slide-container">
+        <div className="slide-image">
+          <img src="https://speakzeasy.files.wordpress.com/2015/05/twa_blogpic_timisoara-4415.jpg" />
+        </div>
+        <div className="carousel-controls">
+          <label for="img-2" className="prev-slide">
+            <span>&lsaquo;</span>
+          </label>
+          <label for="img-1" className="next-slide">
+            <span>&rsaquo;</span>
+          </label>
+        </div>
+      </li>
+      <div className="carousel-dots">
+        <label for="img-1" className="carousel-dot" id="img-dot-1"></label>
+        <label for="img-2" className="carousel-dot" id="img-dot-2"></label>
+        <label for="img-3" className="carousel-dot" id="img-dot-3"></label>
       </div>
-      <nav className="carousel__nav">
-        <ul>
-          <li>
-            <a href="#item-1" aria-label="Item 1">
-              <svg viewBox="0 0 10 10" width="1em">
-                <circle cx="5" cy="5" r="5" fill="#fff" />
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a href="#item-2" aria-label="Item 2">
-              <svg viewBox="0 0 10 10" width="1em">
-                <circle cx="5" cy="5" r="5" fill="#fff" />
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a href="#item-3" aria-label="Item 3">
-              <svg viewBox="0 0 10 10" width="1em">
-                <circle cx="5" cy="5" r="5" fill="#fff" />
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a href="#item-4" aria-label="Item 4">
-              <svg viewBox="0 0 10 10" width="1em">
-                <circle cx="5" cy="5" r="5" fill="#fff" />
-              </svg>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    </ul>
   </div>;
 };
 export default Slider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,34 @@
-import About from '../components/about/About';
-import CelebrityCard from '../components/celebrityCard/CelebrityCard';
-import Slider from '../components/mainCarousel/Slider.jsx';
-import RevolveCarousel from '../components/revolveCarousel/revolveCarousel';
+import About from "../components/about/About";
+import CelebrityCard from "../components/celebrityCard/CelebrityCard";
+import Slider from "../components/mainCarousel/Slider";
+import RevolveCarousel from "../components/revolveCarousel/revolveCarousel";
 
 const Home = () => {
-	return (
-		<div>
-			<div className="min-h-screen flex justify-center items-center bg-sea1 bg-fixed bg-no-repeat bg-cover bg-center">
-				<h1 className="font-black text-6xl">TECHOFES</h1>
-			</div>
+  return (
+    <div>
+      <div className="min-h-screen flex justify-center items-center bg-sea1 bg-fixed bg-no-repeat bg-cover bg-center">
+        <h1 className="font-black text-6xl">TECHOFES</h1>
+      </div>
 
-			<div className="p-10">
-				<About />
-			</div>
+      <div className="p-10">
+        <About />
+      </div>
 
-			<div className="bg-sea2 min-h-screen bg-center bg-no-repeat bg-cover bg-fixed">
-				<div className="mt-20">
-					<div className="text-center text-6xl">Celebrities </div>
-					<div className="mt-20 w-full flex justify-center items-center">
-						<CelebrityCard key="card1" />
-					</div>
-				</div>
-			</div>
-			<RevolveCarousel />
+      <div className="bg-sea2 min-h-screen bg-center bg-no-repeat bg-cover bg-fixed">
+        <div className="mt-20">
+          <div className="text-center text-6xl">Celebrities </div>
+          <div className="mt-20 w-full flex justify-center items-center">
+            <CelebrityCard key="card1" />
+          </div>
+        </div>
+      </div>
+      <RevolveCarousel />
 
-			<div className="bg-sea3 min-h-screen bg-fixed bg-no-repeat bg-cover">
-				<Slider />
-			</div>
-		</div>
-	);
+      <div className="bg-sea3 min-h-screen bg-fixed bg-no-repeat bg-cover">
+        <Slider />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
