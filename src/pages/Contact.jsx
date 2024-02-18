e7f222f1cd3a0fff7ef4a393e120bcf99db69d19
unknown <aravindkumar40m@gmail.com>
2024-02-18 12:18:41 +0530

    register_background,sponser_page,contact_page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,58 +1,57 @@
-import { FaInstagram, FaFacebook, FaLinkedin } from 'react-icons/fa';
-import { CgMail } from 'react-icons/cg';
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import snowParticleConfig from "../particles/snowParticleConfig";
 
 const Contact = () => {
-	return (
-		<div>
-			<div className="h-screen flex justify-center items-center">
-				<div className="justify-center items-center h-[600px] bg-gray-500 w-[800px] rounded-2xl p-4 shadow-2xl">
-					<div className="flex-auto m-2">
-						<h1 className="text-xl m-2">CONTACT</h1>
-						<p className="text-sm md:text-base px-4">Lorem ipsum - +91 1234567890</p>
-						<p className="text-sm md:text-base px-4">Lorem ipsum - +91 1234567890</p>
-					</div>
-					<div className="flex-auto m-2">
-						<h1 className="text-xl m-2">EMAIL</h1>
-						<p className="text-sm md:text-base px-4">
-							Lorem ipsum - <span className="underline">techofes1@gmail.com</span>
-						</p>
-						<p className="text-sm md:text-base px-4">
-							Lorem ipsum - <span className="underline">techofes2@gmail.com</span>
-						</p>
-					</div>
-					<div className="flex-auto m-2">
-						<h1 className="text-xl m-2">CONNECT</h1>
-						<div className="flex gap-5 text-4xl px-4">
-							<a href="#">
-								<FaInstagram />
-							</a>
-							<a href="#">
-								<FaFacebook />
-							</a>
-							<a href="#">
-								<FaLinkedin />
-							</a>
-							<a href="#">
-								<CgMail />
-							</a>
-						</div>
-					</div>
-					<div className="flex justify-center items-center py-5">
-						<div className="h-[190px] w-[500px]  border-solid border-blue-700 border-2 rounded-xl overflow-hidden relative">
-							<iframe
-								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.3878496259977!2d80.23277747515768!3d13.010955587308075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52679fd80e657f%3A0x9727dde0ba49c84e!2sAnna%20University!5e0!3m2!1sen!2sin!4v1707907549033!5m2!1sen!2sin"
-								width="500"
-								height="250"
-								allowFullScreen=""
-								loading="lazy"
-								referrerPolicy="no-referrer-when-downgrade"
-							></iframe>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  async function loadParticles(main) {
+    await loadSlim(main);
+  }
+
+  return (
+    <div>
+      <Particles
+        id="tsparticles"
+        init={loadParticles}
+        options={snowParticleConfig}
+      />
+      <div className="h-screen flex justify-center items-center text-white">
+        <div className="justify-center items-center h-[600px]  w-[800px] rounded-2xl p-4 shadow-2xl relative border-4">
+          <div className="flex-auto m-2 border-4 w-[330px] p-4 rounded-2xl absolute top-[60px]">
+            <h1 className="text-xl flex justify-center">CONTACT</h1>
+            <p className="text-sm md:text-base p-4">
+              Lorem ipsum - +91 1234567890
+            </p>
+            <p className="text-sm md:text-base px-4">
+              Lorem ipsum - +91 1234567890
+            </p>
+          </div>
+          <div className="flex-auto m-2 border-4 w-[340px] p-4 rounded-2xl absolute left-[400px] bottom-[368px]">
+            <h1 className="text-xl m-2 flex justify-center">EMAIL</h1>
+            <p className="text-sm md:text-base px-4 py-1">
+              Lorem ipsum -{" "}
+              <span className="underline">techofes1@gmail.com</span>
+            </p>
+            <p className="text-sm md:text-base px-4 py-1">
+              Lorem ipsum -{" "}
+              <span className="underline">techofes2@gmail.com</span>
+            </p>
+          </div>
+          <div className="flex justify-center items-center py-5 absolute top-[300px] left-[130px] ">
+            <div className="h-[190px] w-[500px]   border-2 rounded-xl overflow-hidden relative">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.3878496259977!2d80.23277747515768!3d13.010955587308075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52679fd80e657f%3A0x9727dde0ba49c84e!2sAnna%20University!5e0!3m2!1sen!2sin!4v1707907549033!5m2!1sen!2sin"
+                width="500"
+                height="250"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -1,12 +1,51 @@
-import Card from '../components/Card';
+import sponser_1 from "../assets/sponsers/sponser_1.jpg";
+import sponser_2 from "../assets/sponsers/sponser_2.jpg";
+import sponser_3 from "../assets/sponsers/sponser_3.jpg";
+import sponser_4 from "../assets/sponsers/sponser_4.jpg";
+import sponser_5 from "../assets/sponsers/sponser_5.jpg";
+
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import snowParticleConfig from "../particles/snowParticleConfig";
 
 const Sponsors = () => {
-	return (
-		<div>
-			Our Sponsors
-			<Card />
-		</div>
-	);
+  const imgs = [sponser_1, sponser_2, sponser_3, sponser_4, sponser_5];
+
+  async function loadParticles(main) {
+    await loadSlim(main);
+  }
+
+  return (
+    <div>
+      <Particles
+        id="tsparticles"
+        init={loadParticles}
+        options={snowParticleConfig}
+      />
+      <div className=" h-full  pb-11">
+        <div className="flex items-center justify-center p-11 text-4xl text-white">
+          <h1>OUR SPONSERS</h1>
+        </div>
+
+        <div className="grid grid-cols-3 gap-10 relative w-[1000px] left-[250px]  border-4 p-11 rounded-xl">
+          {imgs.map((img, index) => (
+            <div className="h-[300px] w-[300px]  rounded-xl hover:size-[310px] relative top-5 border-2">
+              <img
+                key={index}
+                src={img}
+                alt={`Sponsor ${index + 1}`}
+                className="rounded-xl h-[200px] w-[400px]"
+              />
+              <div className="p-2 relative left-[80px] text-2xl text-white">
+                <h1 className="pb-3 pl-5">Name</h1>
+                <p>Description</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sponsors;
