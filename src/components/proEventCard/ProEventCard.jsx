fde62654f46f31e8908298a2592031f072057eb9
unknown <aravindkumar40m@gmail.com>
2024-02-24 23:01:40 +0530

    Added coming soon component

--- src/components/proEventCard/ProEventCard.jsx
+++ src/components/proEventCard/ProEventCard.jsx
@@ -3,6 +3,7 @@ import pro1 from "./../../assets/pro events/pro1.svg";
 import pro2 from "./../../assets/pro events/techofes-awards.png";
 import pro3 from "./../../assets/pro events/talent-of-techofes.png";
 import "./ProEventCard.css";
+import "./TextAnimation.css";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -12,7 +13,7 @@ const ProEventCard = () => {
     AOS.init({
       offset: 200,
       duration: 600,
-      easing: "ease-in-sine",
+      easing: "ease-in-out-quart",
       delay: 100,
     });
   }, []);
@@ -24,13 +25,16 @@ const ProEventCard = () => {
         data-aos="fade-down-right"
       >
         <div className="absolute -top-10 -left-10 z-0 rounded-full w-20 h-20 bg-orange-400" />
-        <img src={pro1} className="absolute bottom-4 left-10" alt="pro1" />
+        {/* {<img src={pro1} className="absolute bottom-4 left-10" alt="pro1" />
         <h1
           id="title"
           className="absolute -bottom-0 w-full text-6xl text-center"
         >
           CONCERT NIGHT
-        </h1>
+        </h1> */}
+        <div className="m-5 py-5">
+          <h1 className="text">Coming soon...</h1>
+        </div>
       </div>
 
       <div
@@ -38,13 +42,9 @@ const ProEventCard = () => {
         data-aos="fade-up"
       >
         <div className="absolute -top-10 left-1/3 z-0 rounded-full w-20 h-20 bg-violet-600" />
-        <img src={pro2} className="absolute bottom-4 left-10" alt="pro1" />
-        <h1
-          id="title"
-          className="absolute -bottom-0 w-full text-6xl text-center"
-        >
-          TECHOFES AWARDS
-        </h1>
+        <div className="m-5 py-5">
+          <h1 className="text">Coming soon...</h1>
+        </div>
       </div>
 
       <div
@@ -52,13 +52,16 @@ const ProEventCard = () => {
         data-aos="fade-down-left"
       >
         <div className="absolute -top-10 -right-10 z-0 rounded-full w-20 h-20 bg-sky-400" />
-        <img src={pro3} className="absolute bottom-4 left-10" alt="pro1" />
+        {/* {<img src={pro3} className="absolute bottom-4 left-10" alt="pro1" />
         <h1
           id="title"
           className="absolute -bottom-0 w-full text-6xl text-center"
         >
           TALENT OF TECHOFES
-        </h1>
+        </h1> */}
+        <div className="m-5 py-5">
+          <h1 className="text">Coming soon...</h1>
+        </div>
       </div>
     </div>
   );
