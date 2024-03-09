65f90de54fea62b49a1caa03a3f10801cabdf621
praveen-221 <praveen-221>
2024-03-09 13:19:07 +0530

    added accomodation

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -10,7 +10,8 @@ const Accomodation = () => {
 					<h1 className='font-anta md:text-2xl text-lg '>
 						Any troubles on where to stay? We have you covered. We have
 						accommodation options to ensure a comfortable and hassle-free
-						experience for all of you.
+						experience for all of you.<br></br>
+						<span className="text-black text-base">** You can also register on-spot in the accommodation desk during the fest **</span>
 					</h1>
 					<h2 className='font-anta md:text-2xl text-lg font-bold mt-16 '>
 						WITH FOOD - ₹400
@@ -20,9 +21,13 @@ const Accomodation = () => {
 					</h2>
 				</div>
 				<div className='sm:mt-20 mt-10 flex flex-wrap sm:w-full  justify-evenly sm:justify-center sm:gap-8 gap-6 text-white'>
-					<p className='sm:px-8 px-4 py-2 sm:py-4 text-lg  sm:text-2xl bg-cyan-800 rounded-2xl'>
-						Registrations open soon
-					</p>
+					<button
+						className="bg-gray-700 sm:px-8 px-4 py-2 sm:py-4 text-lg sm:text-2xl rounded-2xl"
+					>
+						<a href="https://forms.gle/5ctFd3Vb8Cfz8Ymo7" target="_blank" style={{textDecoration: "none"}}>
+							Avail your music room
+						</a>
+					</button>
 				</div>
 			</div>
 		</div>
