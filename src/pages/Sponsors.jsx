39d240c982cea6f2f145a6aff31a4eecebe8c910
Metta Surendhar <msurendhar8815@gmail.com>
2024-03-13 01:34:54 +0530

    added all the sponsors logo

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -3,6 +3,17 @@ import sflogo from '../assets/sponsers/sflogo.png';
 import mk from '../assets/sponsers/mk.jpg';
 import touriga from '../assets/sponsers/touriga.png';
 import zomato from '../assets/sponsers/Zomato_logo.png';
+import ads_123 from '../assets/sponsers/123_logo.jpg';
+import aram_logo from '../assets/sponsers/aram_logo.jpg';
+import flantz from '../assets/sponsers/FLANTZ.jpg';
+import fuji from '../assets/sponsers/fuji_logo.jpg';
+import ishwaryam from '../assets/sponsers/Ishwaryam-02.png';
+import laurels from '../assets/sponsers/LAURELS.png';
+import rajams from '../assets/sponsers/rajams-print.png';
+import zebronics from '../assets/sponsers/zebronics.jpg';
+import toyota from '../assets/sponsers/Toyota-01.jpg';
+import vidiem from '../assets/sponsers/Vidiem.jpg';
+
 import './Merchandise.css';
 
 const Sponsors = () => {
@@ -12,23 +23,35 @@ const Sponsors = () => {
 				<h1 className='text-3xl md:text-5xl font-anta'>OUR SPONSORS</h1>
 			</div>
 
-			<div className='flex text-white flex-col md:flex-row bk  justify-evenly w-[95%] py-8 mx-auto flex-wrap'>
+			<div className='flex text-white flex-col md:flex-row bk  justify-evenly w-[95%] py-16 mx-auto flex-wrap gap-x-20 gap-y-20'>
 				<div>
 					<a href='#'>
 						<img
-							className='h-60 w-60 hover:scale-110 transition-all mx-auto'
-							src={canvas}
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={aram_logo}
 							alt=''
 						/>
 					</a>
 					<h3 className='text-center text-lg p-4 font-anta font-bold'>
-						Creative Technology Partner
+						Associate Partner
 					</h3>
 				</div>
 				<div>
 					<a href='#'>
 						<img
-							className='h-60 w-60 hover:scale-110 transition-all mx-auto'
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={zomato}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Apparel Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className='h-60 w-60 hover:scale-110 transition-all mx-auto object-contain'
 							src={sflogo}
 							alt=''
 						/>
@@ -40,7 +63,115 @@ const Sponsors = () => {
 				<div>
 					<a href='#'>
 						<img
-							className='h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl'
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={zebronics}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Gaming Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={ads_123}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Printing Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={rajams}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Printing Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={toyota}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Automobile Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={flantz}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						event Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={laurels}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Awards & Trophy Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={fuji}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Moments Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain '
+							src={ishwaryam}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Gift Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
+							src={vidiem}
+							alt=''
+						/>
+					</a>
+					<h3 className='text-center text-lg p-4 font-anta font-bold'>
+						Appliances Partner
+					</h3>
+				</div>
+				<div>
+					<a href='#'>
+						<img
+							className='h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
 							src={mk}
 							alt=''
 						/>
@@ -52,7 +183,7 @@ const Sponsors = () => {
 				<div>
 					<a href='#'>
 						<img
-							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl'
+							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl object-contain'
 							src={touriga}
 							alt=''
 						/>
@@ -64,13 +195,13 @@ const Sponsors = () => {
 				<div>
 					<a href='#'>
 						<img
-							className=' h-60 w-60 hover:scale-110 transition-all mx-auto rounded-2xl'
-							src={zomato}
+							className='h-60 w-60 hover:scale-110 transition-all mx-auto object-contain'
+							src={canvas}
 							alt=''
 						/>
 					</a>
 					<h3 className='text-center text-lg p-4 font-anta font-bold'>
-						Apparel Partner
+						Creative Technology Partner
 					</h3>
 				</div>
 			</div>
