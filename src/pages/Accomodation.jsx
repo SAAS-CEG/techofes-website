02418ecbcde02cee2a6d88badd20b1a7d8d14d35
praveen-221 <praveen-221>
2024-03-13 09:02:38 +0530

    minor tweaks

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -10,12 +10,12 @@ const Accomodation = () => {
 	  
 	return (
 		<div>
-			<div className='h-full lg:min-h-[60vh] min-h-[85vh] pb-11 mt-20 cursor-default bg-cover bg-g bg-no-repeat'>
+			<div className='h-full lg:min-h-[60vh] min-h-[85vh] w-[100%] pb-11 mt-20 cursor-default bg-cover bg-g bg-no-repeat'>
 				<div className='flex items-center justify-center p-11 text-3xl md:text-5xl text-white font-anta'>
 					<h1 className='font-anta font-bold'>ACCOMMODATION</h1>
 				</div>
 
-				<div className='flex flex-col justify-center md:p-10 mb-20 bk w-3/5 m-auto p-8 text-white'>
+				<div className='flex flex-col justify-center md:p-10 mb-20 bk w-4/5 md:w-3/5 m-auto p-5 text-white'>
 					<h1 className='font-anta md:text-2xl text-lg '>
 						Any troubles on where to stay? We have you covered. We have
 						accommodation options to ensure a comfortable and hassle-free
